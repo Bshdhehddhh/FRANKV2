172d566aed4fc2c1625265d74799e4de511d60c1
Bshdhehddhh <114882092+Bshdhehddhh@users.noreply.github.com>
2022-11-17 16:36:49 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,24 +54,24 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['201121968769'] //ur owner number
-global.ownername = "𝐾𝐻𝐻𝐹𝑈" //ur owner name
+global.vcardowner = ['201508309376'] //ur owner number
+global.ownername = "𝙁𝙍𝘼𝙉𝙆" //ur owner name
 global.ytname = "YT: khufu" //ur yt chanel name
 global.socialm = "o" //ur github or insta name
 global.location = "Egypt" //ur location
 
 //bot bomdy 
-global.owner = ['201121968769']
-global.ownertag = '201121968769' //ur tag number
+global.owner = ['201508309376']
+global.ownertag = '201508309376' //ur tag number
 
 global.botname = 'ᴹᴿ᭄ Bot Whatsapp ×፝֟͜×' //ur bot name
-global.linkz = "https://youtube.com/channel/UCaWoQPfLiIY4dDp3WeWm7oA" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtube.com/channel/UCaWoQPfLiIY4dDp3WeWm7oA" //ur website to be displayed
-global.botscript = 'https://youtube.com/channel/UCB3Z3phhszGj8FBdaJmIdJQ' //script link
+global.linkz = "https://www.youtube.com/channel/UCF8h6rXfjy1vBklTHyx3Fxg" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UCF8h6rXfjy1vBklTHyx3Fxg" //ur website to be displayed
+global.botscript = 'https://www.youtube.com/channel/UCF8h6rXfjy1vBklTHyx3Fxg' //script link
 global.reactmoji = "💗" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "01121968769" //ur sticker watermark packname
-global.author = "خوفو ص المزعجين ☺️ " //ur sticker watermark author
+global.packname = "01508309376" //ur sticker watermark packname
+global.author = "فرانك ص المزعجين ☺️ " //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./KhufuMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -80,7 +80,7 @@ global.err4r = fs.readFileSync("./KhufuMedia/theme/cheemspic.jpg") //ur error pi
 global.thumb = fs.readFileSync("./KhufuMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['201121968769'] //ur premium numbers
+global.premium = ['201508309376'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
